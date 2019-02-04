534870f0e83e688c9514e5835565a26308ea0568
MircoT <MircoT@users.noreply.github.com>
2019-02-04 14:46:22 +0100

    Fix key formatter

--- tasks/scripts/fix_key.py
+++ tasks/scripts/fix_key.py
@@ -3,16 +3,21 @@ from sys import argv
 
 key_file = argv[1]
 
+print("[FIX KEY] Read key and count lines...")
 with open(key_file) as key_:
     content = key_.read()
     lines = content.count('\n')
 
-if lines == 1:
+if lines <= 1:
+    print("[FIX KEY] KEY MALFORMED - Single line")
     content = content.split()
     new_key = ''
     new_key += '{} {} {} {}\n'.format(*content[:4])
     new_key += '\n'.join(content[4:-4])
     new_key += '\n{} {} {} {}\n'.format(*content[-4:])
 
+    print("[FIX KEY] KEY REWRITTEN...")
     with open(key_file, 'w') as key_:
         key_.write(new_key)
+else:
+    print("[FIX KEY] Everything ok!")
